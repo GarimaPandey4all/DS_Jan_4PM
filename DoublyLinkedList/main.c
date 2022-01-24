8da7d993a4183666060a7a8c78244899b306edf4
Garima Pandey <59961721+GarimaPandey4all@users.noreply.github.com>
2022-01-24 17:24:33 +0530

    Add files via upload

--- DoublyLinkedList/main.c
+++ DoublyLinkedList/main.c
@@ -4,11 +4,40 @@
 struct node {
 
     struct node *prev;
-    int info
+    int info;
     struct node *next;
 };
 
 struct node *start = NULL;
+
+struct node *searchNode()
+{
+    struct node *t;
+    int search;
+
+    if(start == NULL)
+    {
+        return NULL;
+    }
+    else {
+        printf("Enter any value to be search:");
+        scanf("%d", &search);
+
+        t = start;
+
+        while(t != NULL)
+        {
+            if(t->info == search)
+            {
+                return t;
+            }
+
+            t = t->next;
+        }
+    }
+
+    return NULL;
+};
 
 struct node *createNode()
 {
@@ -100,9 +129,154 @@ void insertAfterNode()
     }
 }
 
+void viewList()
+{
+    struct node *t;
+
+    if(start == NULL)
+    {
+        printf("List is Empty");
+    }
+    else {
+
+        t = start;
+
+        while(t != NULL)
+        {
+            printf("%d  ", t->info);
+            t = t->next;
+        }
+    }
+}
+
+void deleteFirstNode()
+{
+    struct node *t;
+
+    if(start == NULL)
+    {
+        printf("List is Empty");
+    }
+    else {
+
+        t = start;
+
+        start = start->next;
+        start->prev = NULL;
+
+        free(t);
+    }
+}
+
+void deleteLastNode()
+{
+    struct node *t;
+
+    if(start == NULL)
+    {
+        printf("List is Empty");
+    }
+    else {
+
+        t = start;
+
+        while(t->next != NULL)
+        {
+            t = t->next;
+        }
+
+        if(start->next == NULL)
+        {
+            start = NULL;
+        }
+        else {
+            t->prev->next = NULL;
+        }
+
+        free(t);
+    }
+
+}
+
+
+void deleteIntermediateNode()
+{
+    struct node *ptr;
+
+    ptr = searchNode();
+
+    if(ptr == NULL)
+    {
+        printf("Invalid Search. List is Empty");
+    }
+
+    else if(ptr->prev == NULL || ptr->next == NULL)
+    {
+        printf("Invalid Deletion");
+    }
+    else {
+        ptr->prev->next = ptr->next;
+        ptr->next->prev = ptr->prev;
+
+        free(ptr);
+    }
+
+}
+
 int main()
 {
+    int choice;
 
+while(1)
+{
 
+    printf("\n\nPress 1. Insert At Start\n");
+    printf("\nPress 2. Insert At Last\n");
+    printf("\nPress 3. Insert After Node\n");
+    printf("\nPress 4. Delete First Node\n");
+    printf("\nPress 5. Delete Last Node\n");
+    printf("\nPress 6. Delete Intermediate Node\n");
+    printf("\nPress 7. ViewList\n");
+    printf("\nPress 8. Exit\n");
+    printf("\n\nEnter Your Choice:\n");
+    scanf("%d", &choice);
+
+    switch(choice)
+    {
+    case 1:
+        insertAtStart();
+        break;
+
+    case 2:
+        insertAtLast();
+        break;
+
+    case 3:
+        insertAfterNode();
+        break;
+
+    case 4:
+        deleteFirstNode();
+        break;
+
+    case 5:
+        deleteLastNode();
+        break;
+
+    case 6:
+        deleteIntermediateNode();
+        break;
+
+    case 7:
+        viewList();
+        break;
+
+    case 8:
+        exit(0);
+
+    default:
+        printf("Invalid Choice");
+    }
+}
     return 0;
 }
